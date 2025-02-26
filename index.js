a2954bf50286bb8c671fd4bfb4c31371af220020
Newness01 <106417742+Newness01@users.noreply.github.com>
2025-02-26 16:30:09 +0100

    I will do it

---
+++ index.js
@@ -0,0 +1,6 @@
+function submitForm() {
+    // Hide the form
+    document.getElementById("myForm").style.display = "none";
+    // Show the message
+    document.getElementById("message").style.display = "block";
+}
